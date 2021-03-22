95815f15b3da23479060b7125e4f5aae0098eaf4
erlanbelekov <belekoverlanhehe@gmail.com>
2021-03-22 10:37:34 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Erlan on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

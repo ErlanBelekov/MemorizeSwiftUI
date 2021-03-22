f8cc70b8dad3bcd03764ba5c7bcaf3a6bbde2304
erlanbelekov <belekoverlanhehe@gmail.com>
2021-03-22 16:51:43 +0100

    change ContentView to EmojiMemoryGameView

---
+++ Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,53 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by Erlan on 3/22/21.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        return HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card)
+                    .onTapGesture {
+                        viewModel.chooseCard(card: card)
+                    }
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+    }
+}

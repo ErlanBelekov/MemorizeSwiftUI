5ed1173cefbc6ef93d3ae6fd2625e3bc161345a0
erlanbelekov <belekoverlanhehe@gmail.com>
2021-03-23 19:44:39 +0100

    create Pie view

---
+++ Memorize/Views/Pie.swift
@@ -0,0 +1,53 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Erlan on 3/23/21.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(startAngle.radians, endAngle.radians)
+        }
+        set {
+            startAngle = Angle.radians(newValue.first)
+            endAngle = Angle.radians(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * cos(CGFloat(startAngle.radians)),
+            y: center.y + radius * cos(CGFloat(startAngle.radians))
+        )
+        
+        path.move(to: center)
+        path.addLine(to: start)
+        path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        path.addLine(to: center)
+        
+        return path
+    }
+}
+
+//struct Pie: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//struct Pie_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Pie()
+//    }
+//}
